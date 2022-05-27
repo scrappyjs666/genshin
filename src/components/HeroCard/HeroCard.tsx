6e8da8ecd5fe9ec3d4dd1376d353c5269d53fbab
scrappyjs666 <82704685+scrappyjs666@users.noreply.github.com>
2022-05-27 23:16:56 +0300

    Added pictures, changed layout and styles, added fonts, working with typescript

--- src/components/HeroCard/HeroCard.tsx
+++ src/components/HeroCard/HeroCard.tsx
@@ -1,19 +1,30 @@
 import styles from './HeroCard.module.scss';
 import React from 'react';
 
+
 interface card {
   img: string,
   title: string,
-  rating: number;
+  btnText: string,
+  imgBtn: string;
+  styleBack?: string | number;
+  onClick: (item: string, i: number) => void;
+  indexBtnActive: Function;
 }
 
-const  HeroCard: React.FC<card> = ({ img, title }) => {
+const  HeroCard: React.FC<card> = ({  title, img, btnText, imgBtn, indexBtnActive }) => {
   return(
     <>
       <div 
         className={styles.heroCard__container}>
-        {title}
-        {img}
+        <h2 className={styles.heroCard__title}>{title}</h2>
+        <img src={img}/>
+        <button
+          onClick={(item: string, i: number)=>  indexBtnActive(item, i)}
+          className={styles.hero__add}>
+          <img src={imgBtn}/>
+          {btnText}
+        </button>
       </div>
     </>
   )
--- src/pages/HomaPage/HomePage.tsx
+++ src/pages/HomaPage/HomePage.tsx
@@ -1,25 +1,45 @@
+/* eslint-disable no-constant-condition */
 import styles from './HomePage.module.scss';
 import React from 'react';
 import { getApiResource } from '../../api/network';
 import { useState, useEffect } from 'react';
 import HeroCard from '../../components/HeroCard/HeroCard';
+import imgBtn from './img/star.svg'
 
 const  HomePage: React.FC = () => {
   const [characters, setCharacters] = useState([]);
-  const url = 'https://api.genshin.dev/weapons/akuoumaru';
+  const [btnActive, setBtnActive] = useState(0);
+  const [cls, setCls] = useState('green');
+
+  const indexBtnActive = (item: string, i:number):void => {
+    console.log('1')
+    setBtnActive(i);
+    console.log(i)
+  }
+
+  const url = 'https://api.genshin.dev/characters';
 
   useEffect(() => {
     getApiResource(url)
       .then((data) => {
         setCharacters(data);
-        console.log(data)
       });
   }, []);
 
+  const excludedData = ['thoma', 'traveler-geo', 'traveler-electro', 'traveler-anemo'];
+
   return(
     <>
-      <HeroCard img = {img}/>
-      <div>HomePage</div>
+      {characters.filter(i => !excludedData.includes(i)).map((item, i) =>
+        (<HeroCard
+          key={item}
+          title={item}
+          img={`./images/characters/${item}/gacha-card`}
+          imgBtn={btnActive ? imgBtn : ''}
+          btnText={btnActive ? 'add to favorites' : 'selected character added'}
+          indexBtnActive={indexBtnActive} onClick={function (item: string, i: number): void {
+            throw new Error('Function not implemented.');
+          } }        />))}
     </>
   )
 }
