77dd5aeaaffb6225e6490f57df0e43de531f8aaf
scrappyjs666 <82704685+scrappyjs666@users.noreply.github.com>
2022-06-29 22:33:50 +0300

    delete

--- src/Store/user/interface.ts
+++
@@ -1,6 +0,0 @@
-export interface IUser {
-  email: string | null
-  id: string | null
-  status: 'loading' | 'success' | 'error'
-  textError: string | undefined
-}
--- src/components/LoginForm/LoginForm.jsx
+++
@@ -1,9 +0,0 @@
-import { useAppSelector } from 'Store/hooks/hooks'
-import FrontCard from './FrontCard'
-import BackCard from './LoginBackCard/BackCard'
-
-export const LoginForm = () => {
-  const { status } = useAppSelector((state) => state.userSlice)
-
-  return <>{status === 'loading' ? <FrontCard /> : <BackCard />}</>
-}
